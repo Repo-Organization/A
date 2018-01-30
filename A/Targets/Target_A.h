c725871594b37680a0caf90454da90cb8adb2c38
gaofei <gaofei11@jd.com>
2018-01-30 12:19:21 +0800

    1.0.0

---
+++ A/Targets/Target_A.h
@@ -0,0 +1,15 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by gaofei on 1/30/18.
+//  Copyright © 2018 gaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
+@end
